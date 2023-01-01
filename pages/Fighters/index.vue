1f45aceec3dbb3a7bf5f8cecdf108c6f84aeb859
Vince489 <vincentcornelius489@gmail.com>
2023-01-01 18:01:20 -0500

    update

--- pages/Fighters/index.vue
+++ pages/Fighters/index.vue
@@ -1,19 +1,66 @@
 <template>
-  <div>
-    <h2>Fighters</h2>
-  </div>
 
-  <!-- <div class="grid grid-cols-4 gap-5">
-    <div v-for="g in gamers">
-      <nuxt-link :to="`/gamers/${g.password}`">{{ g.gamerTag }}</nuxt-link>
+  <div class="p-4 boxer-card-container rounded max-w-xs bg-gray-200">
+
+    <div class="boxer-name font-bold pb-2 text-center text-gray-700">
+      <h2>Errol Spence Jr</h2>
     </div>
-  </div>   -->
+
+    <div>
+      <img src="https://www.premierboxingchampions.com/sites/default/files/styles/responsive_764_x_430/public/fighter-primary-image-16-9/WideHeadshot_SpenceJr.jpg" alt="me">
+    </div>
+
+    <div>
+
+      <div class="boxer-name font-bold py-2 text-center text-gray-700">
+        <h3>@OffgridVince</h3>
+      </div>
+
+      <div class="text-center record gap-1 flex justify-center font-bold text-white">
+        
+        <div class="wins bg-green-600 py-1 px-6 rounded">
+          <p>28</p>
+          <span>WINS</span>                 
+        </div>
+
+        <div class="losses bg-red-600 p-1 px-6 rounded">
+          <p>0</p>
+          <span>LOSSES</span>          
+        </div>
+
+        <div class="draws bg-blue-400 p-1 px-6 rounded">
+          <p>0</p>
+          <span>DRAWS</span>          
+        </div>
+
+      </div>  
+
+      <div>   
+        <div class=" pt-2 justify-center record flex gap-4">
+          <div class="wins text-green-700">
+            <p>22<span> KO WINS</span></p>        
+          </div>
+          <div class="losses text-red-700">
+            <p>0<span> KO LOSSES</span></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="pt-4 text-center">
+        <p class="font-semibold">Welterweight</p>
+        <p>HT- 5' 9", WT- 146 lbs, RCH- 72"</p>
+      </div>
+
+    </div>
+  </div>
+  
 </template>
 
 <script setup>
-  // const { data: gamers } = await useFetch('http://localhost:4000/gamers')
+
 </script>
 
 <style scoped>
 
 </style>
+
