8fff6415db7dfd271ee565478809c2e149d45a1a
Vince489 <vincentcornelius489@gmail.com>
2023-01-01 21:27:30 -0500

    responsive

--- pages/Fighters/index.vue
+++ pages/Fighters/index.vue
@@ -1,58 +1,464 @@
 <template>
 
-  <div class="p-4 boxer-card-container rounded max-w-xs bg-gray-200">
+<div class="md:grid grid-cols-4 m-auto">
 
-    <div class="boxer-name font-bold pb-2 text-center text-gray-700">
-      <h2>Errol Spence Jr</h2>
+  <div class="boxer-card-container p-6 mb-6 rounded max-w-xs bg-gray-200">
+
+      <div class="boxer-name font-bold pb-2 text-center text-gray-700">
+        <h2>Corby Baker</h2>
+      </div>
+
+      <div class="w-48 h-64 m-auto">
+        <img src="https://scontent.ftpa1-1.fna.fbcdn.net/v/t39.30808-6/313187432_185175870744715_5901823652848492809_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=gQ55nRNUI-kAX_8U8d0&_nc_ht=scontent.ftpa1-1.fna&oh=00_AfD0xaHsbVl9nqgeotnobcXH_CHewYU07Safmt6AFc7tRw&oe=63B81004" alt="me">
+      </div>
+
+      <div>
+
+        <div class="boxer-name font-bold py-2 text-center text-gray-700">
+          <h3>@OffgridVince</h3>
+        </div>
+
+        <div class="text-center record gap-1 flex justify-center font-bold text-white">
+          
+          <div class="wins bg-green-600 py-1 px-6 rounded">
+            <p>0</p>
+            <span>WINS</span>                 
+          </div>
+
+          <div class="losses bg-red-600 p-1 px-6 rounded">
+            <p>0</p>
+            <span>LOSSES</span>          
+          </div>
+
+          <div class="draws bg-blue-400 p-1 px-6 rounded">
+            <p>0</p>
+            <span>DRAWS</span>          
+          </div>
+
+        </div>  
+
+        <div>   
+          <div class=" pt-2 justify-center record flex gap-4">
+            <div class="wins text-green-700">
+              <p>0<span> KO WINS</span></p>        
+            </div>
+            <div class="losses text-red-700">
+              <p>0<span> KO LOSSES</span></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="pt-4 text-center">
+          <p class="font-semibold">Welterweight</p>
+          <p>HT- 5' 9", WT- 146 lbs, RCH- 72"</p>
+        </div>
+
+        <div class="mt-3 btn2 text-center">View Fighter</div>
+
+      </div>
+  </div>
+
+  <div class="boxer-card-container p-6 mb-6 rounded max-w-xs bg-gray-200">
+
+  <div class="boxer-name font-bold pb-2 text-center text-gray-700">
+    <h2>Clubber Lang</h2>
+  </div>
+
+  <div class="w-48 m-auto h-64">
+    <img src="https://scontent.ftpa1-2.fna.fbcdn.net/v/t39.30808-6/313197716_185179717410997_9056183693428324314_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=XCSyH1zwr3sAX_vLcdm&_nc_ht=scontent.ftpa1-2.fna&oh=00_AfDBrqcWXOWVnnXQNH8PlF2h9UdTqcTqkdalHLpFpgThQg&oe=63B7FA73" alt="me">
+  </div>
+
+  <div>
+
+    <div class="boxer-name font-bold py-2 text-center text-gray-700">
+      <h3>@WING CHUN175879</h3>
     </div>
 
-    <div>
-      <img src="https://www.premierboxingchampions.com/sites/default/files/styles/responsive_764_x_430/public/fighter-primary-image-16-9/WideHeadshot_SpenceJr.jpg" alt="me">
+    <div class="text-center record gap-1 flex justify-center font-bold text-white">
+      
+      <div class="wins bg-green-600 py-1 px-6 rounded">
+        <p>1</p>
+        <span>WINS</span>                 
+      </div>
+
+      <div class="losses bg-red-600 p-1 px-6 rounded">
+        <p>0</p>
+        <span>LOSSES</span>          
+      </div>
+
+      <div class="draws bg-blue-400 p-1 px-6 rounded">
+        <p>0</p>
+        <span>DRAWS</span>          
+      </div>
+
+    </div>  
+
+    <div>   
+      <div class=" pt-2 justify-center record flex gap-4">
+        <div class="wins text-green-700">
+          <p>1<span> KO WINS</span></p>        
+        </div>
+        <div class="losses text-red-700">
+          <p>0<span> KO LOSSES</span></p>
+        </div>
+      </div>
     </div>
 
-    <div>
+    <div class="pt-4 text-center">
+      <p class="font-semibold">Middleweight</p>
+      <p>HT- 6' 0", WT- 155 lbs, RCH- 75"</p>
+    </div>
 
-      <div class="boxer-name font-bold py-2 text-center text-gray-700">
-        <h3>@OffgridVince</h3>
+    <div class="mt-3 btn2 text-center">View Fighter</div>
+
+  </div>
+
+
+  </div>
+
+  <div class="boxer-card-container p-6 mb-6 rounded max-w-xs bg-gray-200">
+
+<div class="boxer-name font-bold pb-2 text-center text-gray-700">
+  <h2>Corby Baker</h2>
+</div>
+
+<div class="w-48 h-64 m-auto">
+  <img src="https://scontent.ftpa1-1.fna.fbcdn.net/v/t39.30808-6/313187432_185175870744715_5901823652848492809_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=gQ55nRNUI-kAX_8U8d0&_nc_ht=scontent.ftpa1-1.fna&oh=00_AfD0xaHsbVl9nqgeotnobcXH_CHewYU07Safmt6AFc7tRw&oe=63B81004" alt="me">
+</div>
+
+<div>
+
+  <div class="boxer-name font-bold py-2 text-center text-gray-700">
+    <h3>@OffgridVince</h3>
+  </div>
+
+  <div class="text-center record gap-1 flex justify-center font-bold text-white">
+    
+    <div class="wins bg-green-600 py-1 px-6 rounded">
+      <p>0</p>
+      <span>WINS</span>                 
+    </div>
+
+    <div class="losses bg-red-600 p-1 px-6 rounded">
+      <p>0</p>
+      <span>LOSSES</span>          
+    </div>
+
+    <div class="draws bg-blue-400 p-1 px-6 rounded">
+      <p>0</p>
+      <span>DRAWS</span>          
+    </div>
+
+  </div>  
+
+  <div>   
+    <div class=" pt-2 justify-center record flex gap-4">
+      <div class="wins text-green-700">
+        <p>0<span> KO WINS</span></p>        
       </div>
-
-      <div class="text-center record gap-1 flex justify-center font-bold text-white">
-        
-        <div class="wins bg-green-600 py-1 px-6 rounded">
-          <p>28</p>
-          <span>WINS</span>                 
-        </div>
-
-        <div class="losses bg-red-600 p-1 px-6 rounded">
-          <p>0</p>
-          <span>LOSSES</span>          
-        </div>
-
-        <div class="draws bg-blue-400 p-1 px-6 rounded">
-          <p>0</p>
-          <span>DRAWS</span>          
-        </div>
-
-      </div>  
-
-      <div>   
-        <div class=" pt-2 justify-center record flex gap-4">
-          <div class="wins text-green-700">
-            <p>22<span> KO WINS</span></p>        
-          </div>
-          <div class="losses text-red-700">
-            <p>0<span> KO LOSSES</span></p>
-          </div>
-        </div>
+      <div class="losses text-red-700">
+        <p>0<span> KO LOSSES</span></p>
       </div>
-
-      <div class="pt-4 text-center">
-        <p class="font-semibold">Welterweight</p>
-        <p>HT- 5' 9", WT- 146 lbs, RCH- 72"</p>
-      </div>
-
     </div>
   </div>
+
+  <div class="pt-4 text-center">
+    <p class="font-semibold">Welterweight</p>
+    <p>HT- 5' 9", WT- 146 lbs, RCH- 72"</p>
+  </div>
+
+  <div class="mt-3 btn2 text-center">View Fighter</div>
+
+</div>
+  </div>
+
+  <div class="boxer-card-container p-6 mb-6 rounded max-w-xs bg-gray-200">
+
+<div class="boxer-name font-bold pb-2 text-center text-gray-700">
+<h2>Errol Spence Jr</h2>
+</div>
+
+<div class="w-48 m-auto h-64">
+<img src="https://scontent.ftpa1-2.fna.fbcdn.net/v/t39.30808-6/313197716_185179717410997_9056183693428324314_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=XCSyH1zwr3sAX_vLcdm&_nc_ht=scontent.ftpa1-2.fna&oh=00_AfDBrqcWXOWVnnXQNH8PlF2h9UdTqcTqkdalHLpFpgThQg&oe=63B7FA73" alt="me">
+</div>
+
+<div>
+
+<div class="boxer-name font-bold py-2 text-center text-gray-700">
+<h3>@WING</h3>
+</div>
+
+<div class="text-center record gap-1 flex justify-center font-bold text-white">
+
+<div class="wins bg-green-600 py-1 px-6 rounded">
+  <p>28</p>
+  <span>WINS</span>                 
+</div>
+
+<div class="losses bg-red-600 p-1 px-6 rounded">
+  <p>0</p>
+  <span>LOSSES</span>          
+</div>
+
+<div class="draws bg-blue-400 p-1 px-6 rounded">
+  <p>0</p>
+  <span>DRAWS</span>          
+</div>
+
+</div>  
+
+<div>   
+<div class=" pt-2 justify-center record flex gap-4">
+  <div class="wins text-green-700">
+    <p>22<span> KO WINS</span></p>        
+  </div>
+  <div class="losses text-red-700">
+    <p>0<span> KO LOSSES</span></p>
+  </div>
+</div>
+</div>
+
+<div class="pt-4 text-center">
+<p class="font-semibold">Welterweight</p>
+<p>HT- 5' 9", WT- 146 lbs, RCH- 72"</p>
+</div>
+
+<div class="mt-3 btn2 text-center">View Fighter</div>
+
+</div>
+
+
+  </div>
+
+  <div class="boxer-card-container p-6 mb-6 rounded max-w-xs bg-gray-200">
+
+<div class="boxer-name font-bold pb-2 text-center text-gray-700">
+  <h2>Corby Baker</h2>
+</div>
+
+<div class="w-48 h-64 m-auto">
+  <img src="https://scontent.ftpa1-1.fna.fbcdn.net/v/t39.30808-6/313187432_185175870744715_5901823652848492809_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=gQ55nRNUI-kAX_8U8d0&_nc_ht=scontent.ftpa1-1.fna&oh=00_AfD0xaHsbVl9nqgeotnobcXH_CHewYU07Safmt6AFc7tRw&oe=63B81004" alt="me">
+</div>
+
+<div>
+
+  <div class="boxer-name font-bold py-2 text-center text-gray-700">
+    <h3>@OffgridVince</h3>
+  </div>
+
+  <div class="text-center record gap-1 flex justify-center font-bold text-white">
+    
+    <div class="wins bg-green-600 py-1 px-6 rounded">
+      <p>0</p>
+      <span>WINS</span>                 
+    </div>
+
+    <div class="losses bg-red-600 p-1 px-6 rounded">
+      <p>0</p>
+      <span>LOSSES</span>          
+    </div>
+
+    <div class="draws bg-blue-400 p-1 px-6 rounded">
+      <p>0</p>
+      <span>DRAWS</span>          
+    </div>
+
+  </div>  
+
+  <div>   
+    <div class=" pt-2 justify-center record flex gap-4">
+      <div class="wins text-green-700">
+        <p>0<span> KO WINS</span></p>        
+      </div>
+      <div class="losses text-red-700">
+        <p>0<span> KO LOSSES</span></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="pt-4 text-center">
+    <p class="font-semibold">Welterweight</p>
+    <p>HT- 5' 9", WT- 146 lbs, RCH- 72"</p>
+  </div>
+
+  <div class="mt-3 btn2 text-center">View Fighter</div>
+
+</div>
+  </div>
+
+  <div class="boxer-card-container p-6 mb-6 rounded max-w-xs bg-gray-200">
+
+<div class="boxer-name font-bold pb-2 text-center text-gray-700">
+<h2>Errol Spence Jr</h2>
+</div>
+
+<div class="w-48 m-auto h-64">
+<img src="https://scontent.ftpa1-2.fna.fbcdn.net/v/t39.30808-6/313197716_185179717410997_9056183693428324314_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=XCSyH1zwr3sAX_vLcdm&_nc_ht=scontent.ftpa1-2.fna&oh=00_AfDBrqcWXOWVnnXQNH8PlF2h9UdTqcTqkdalHLpFpgThQg&oe=63B7FA73" alt="me">
+</div>
+
+<div>
+
+<div class="boxer-name font-bold py-2 text-center text-gray-700">
+<h3>@WING</h3>
+</div>
+
+<div class="text-center record gap-1 flex justify-center font-bold text-white">
+
+<div class="wins bg-green-600 py-1 px-6 rounded">
+  <p>28</p>
+  <span>WINS</span>                 
+</div>
+
+<div class="losses bg-red-600 p-1 px-6 rounded">
+  <p>0</p>
+  <span>LOSSES</span>          
+</div>
+
+<div class="draws bg-blue-400 p-1 px-6 rounded">
+  <p>0</p>
+  <span>DRAWS</span>          
+</div>
+
+</div>  
+
+<div>   
+<div class=" pt-2 justify-center record flex gap-4">
+  <div class="wins text-green-700">
+    <p>22<span> KO WINS</span></p>        
+  </div>
+  <div class="losses text-red-700">
+    <p>0<span> KO LOSSES</span></p>
+  </div>
+</div>
+</div>
+
+<div class="pt-4 text-center">
+<p class="font-semibold">Welterweight</p>
+<p>HT- 5' 9", WT- 146 lbs, RCH- 72"</p>
+</div>
+
+<div class="mt-3 btn2 text-center">View Fighter</div>
+
+</div>
+
+
+  </div>
+
+  <div class="boxer-card-container p-6 mb-6 rounded max-w-xs bg-gray-200">
+
+      <div class="boxer-name font-bold pb-2 text-center text-gray-700">
+        <h2>Corby Baker</h2>
+      </div>
+
+      <div class="w-48 h-64 m-auto">
+        <img src="https://scontent.ftpa1-1.fna.fbcdn.net/v/t39.30808-6/313187432_185175870744715_5901823652848492809_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=gQ55nRNUI-kAX_8U8d0&_nc_ht=scontent.ftpa1-1.fna&oh=00_AfD0xaHsbVl9nqgeotnobcXH_CHewYU07Safmt6AFc7tRw&oe=63B81004" alt="me">
+      </div>
+
+      <div>
+
+        <div class="boxer-name font-bold py-2 text-center text-gray-700">
+          <h3>@OffgridVince</h3>
+        </div>
+
+        <div class="text-center record gap-1 flex justify-center font-bold text-white">
+          
+          <div class="wins bg-green-600 py-1 px-6 rounded">
+            <p>0</p>
+            <span>WINS</span>                 
+          </div>
+
+          <div class="losses bg-red-600 p-1 px-6 rounded">
+            <p>0</p>
+            <span>LOSSES</span>          
+          </div>
+
+          <div class="draws bg-blue-400 p-1 px-6 rounded">
+            <p>0</p>
+            <span>DRAWS</span>          
+          </div>
+
+        </div>  
+
+        <div>   
+          <div class=" pt-2 justify-center record flex gap-4">
+            <div class="wins text-green-700">
+              <p>0<span> KO WINS</span></p>        
+            </div>
+            <div class="losses text-red-700">
+              <p>0<span> KO LOSSES</span></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="pt-4 text-center">
+          <p class="font-semibold">Welterweight</p>
+          <p>HT- 5' 9", WT- 146 lbs, RCH- 72"</p>
+        </div>
+
+        <div class="mt-3 btn2 text-center">View Fighter</div>
+
+      </div>
+  </div>
+
+  <div class="boxer-card-container p-6 mb-6 rounded max-w-xs bg-gray-200">
+
+  <div class="boxer-name font-bold pb-2 text-center text-gray-700">
+    <h2>Errol Spence Jr</h2>
+  </div>
+
+  <div class="w-48 m-auto h-64">
+    <img src="https://scontent.ftpa1-2.fna.fbcdn.net/v/t39.30808-6/313197716_185179717410997_9056183693428324314_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=XCSyH1zwr3sAX_vLcdm&_nc_ht=scontent.ftpa1-2.fna&oh=00_AfDBrqcWXOWVnnXQNH8PlF2h9UdTqcTqkdalHLpFpgThQg&oe=63B7FA73" alt="me">
+  </div>
+
+  <div>
+
+    <div class="boxer-name font-bold py-2 text-center text-gray-700">
+      <h3>@WING</h3>
+    </div>
+
+    <div class="text-center record gap-1 flex justify-center font-bold text-white">
+      
+      <div class="wins bg-green-600 py-1 px-6 rounded">
+        <p>28</p>
+        <span>WINS</span>                 
+      </div>
+
+      <div class="losses bg-red-600 p-1 px-6 rounded">
+        <p>0</p>
+        <span>LOSSES</span>          
+      </div>
+
+      <div class="draws bg-blue-400 p-1 px-6 rounded">
+        <p>0</p>
+        <span>DRAWS</span>          
+      </div>
+
+    </div>  
+
+    <div>   
+      <div class=" pt-2 justify-center record flex gap-4">
+        <div class="wins text-green-700">
+          <p>22<span> KO WINS</span></p>        
+        </div>
+        <div class="losses text-red-700">
+          <p>0<span> KO LOSSES</span></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="pt-4 text-center">
+      <p class="font-semibold">Welterweight</p>
+      <p>HT- 5' 9", WT- 146 lbs, RCH- 72"</p>
+    </div>
+
+    <div class="mt-3 btn2 text-center">View Fighter</div>
+
+  </div>
+
+
+  </div>
+
+</div>
   
 </template>
 
