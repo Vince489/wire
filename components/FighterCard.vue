bbdadbe3abf4c18e8748cfe99ba6e6fd323b4c19
Vince489 <vincentcornelius489@gmail.com>
2023-01-18 02:10:22 -0500

    working

--- components/FighterCard.vue
+++ components/FighterCard.vue
@@ -2,6 +2,8 @@
   <div >
     <div class="relative boxer-card-container p-6 mb-6 rounded max-w-xs bg-gray-300">
 
+ <!-- Boxer Name-->   
+
 <div class="boxer-name font-bold pb-4 text-center text-gray-700">
   <h2>Clubber Lang</h2>
 </div>
@@ -13,7 +15,7 @@
 </div>
 
 <div>
-
+<!--Boxer Name-->
   <div class="boxer-name font-bold py-2 text-center text-gray-700">
     <h3>WING</h3>
   </div>
@@ -61,6 +63,126 @@
 
   <div>
     <div class="relative boxer-card-container p-6 mb-6 rounded max-w-xs bg-gray-300">
+
+<div class="boxer-name font-bold pb-4 text-center text-gray-700">
+  <h2>Randall Cobb</h2>
+</div>
+
+<div class="absolute top-0.5 mt-3 left-0.5 ml-3 bg-[#3672bb] font-semibold text-white px-3 py-2 rounded-full text-sm">82</div>
+
+<div class="w-48 h-60 m-auto">
+  <img src="/Ye-removebg-preview.png" alt="me">
+</div>
+
+<div>
+
+  <div class="boxer-name font-bold py-2 text-center text-gray-700">
+    <h3>@Ye Olde Shaq</h3>
+  </div>
+
+  <div class="text-center record gap-1 flex justify-center font-bold text-white">
+    
+    <div class="wins bg-green-600 py-1 px-6 rounded">
+      <p>1</p>
+      <span>WINS</span>                 
+    </div>
+
+    <div class="losses bg-red-600 p-1 px-6 rounded">
+      <p>0</p>
+      <span>LOSSES</span>          
+    </div>
+
+    <div class="draws bg-blue-400 p-1 px-6 rounded">
+      <p>0</p>
+      <span>DRAWS</span>          
+    </div>
+
+  </div>  
+
+  <div>   
+    <div class="font-semibold pt-4 justify-center record flex gap-4">
+      <div class="wins text-green-700">
+        <p>1<span> KO WINS</span></p>        
+      </div>
+      <div class="losses text-red-700">
+        <p>0<span> KO LOSSES</span></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="pt-4 text-center">
+    <p class="font-semibold">Heavyweight</p>
+    <p>HT- 6' 3", WT- 242 lbs, RCH- 78"</p>
+  </div>
+
+  <div class="mt-3 btn2 text-center">View Fighter</div>
+
+</div>
+</div>
+  </div>
+
+  <div >
+    <div class="relative boxer-card-container p-6 mb-6 rounded max-w-xs bg-gray-300">
+
+<div class="boxer-name font-bold pb-4 text-center text-gray-700">
+  <h2>GOD OF WAR</h2>
+</div>
+
+<div class="absolute top-0.5 mt-3 left-0.5 ml-3 bg-[#3672bb] font-semibold text-white px-3 py-2 rounded-full text-sm">90</div>
+
+<div class="w-48 h-60 m-auto">
+  <img src="/him2-removebg-preview.png" alt="me">
+</div>
+
+<div>
+
+  <div class="boxer-name font-bold py-2 text-center text-gray-700">
+    <h3>@GAMERTAG</h3>
+  </div>
+
+  <div class="text-center record gap-1 flex justify-center font-bold text-white">
+    
+    <div class="wins bg-green-600 py-1 px-6 rounded">
+      <p>0</p>
+      <span>WINS</span>                 
+    </div>
+
+    <div class="losses bg-red-600 p-1 px-6 rounded">
+      <p>0</p>
+      <span>LOSSES</span>          
+    </div>
+
+    <div class="draws bg-blue-400 p-1 px-6 rounded">
+      <p>0</p>
+      <span>DRAWS</span>          
+    </div>
+
+  </div>  
+
+  <div>   
+    <div class="font-semibold pt-4 justify-center record flex gap-4">
+      <div class="wins text-green-700">
+        <p>0<span> KO WINS</span></p>        
+      </div>
+      <div class="losses text-red-700">
+        <p>0<span> KO LOSSES</span></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="pt-4 text-center">
+    <p class="font-semibold">Lightweight</p>
+    <p>HT- 6' 0", WT- 135 lbs, RCH- 80"</p>
+  </div>
+
+  <div class="mt-3 btn2 text-center">View Fighter</div>
+
+</div>
+</div>
+  </div>
+
+    <div>
+<div class="relative boxer-card-container p-6 mb-6 rounded max-w-xs bg-gray-300">
 
 <div class="boxer-name font-bold pb-4 text-center text-gray-700">
   <h2>Wata Bully</h2>
@@ -119,68 +241,8 @@
 </div>
   </div>
 
-  <div >
-    <div class="relative boxer-card-container p-6 mb-6 rounded max-w-xs bg-gray-300">
-
-<div class="boxer-name font-bold pb-4 text-center text-gray-700">
-  <h2>GOD OF WAR</h2>
-</div>
-
-<div class="absolute top-0.5 mt-3 left-0.5 ml-3 bg-[#3672bb] font-semibold text-white px-3 py-2 rounded-full text-sm">90</div>
-
-<div class="w-48 h-56 m-auto">
-  <img src="/him2-removebg-preview.png" alt="me">
-</div>
-
-<div>
-
-  <div class="boxer-name font-bold py-2 text-center text-gray-700">
-    <h3>@GAMERTAG</h3>
-  </div>
-
-  <div class="text-center record gap-1 flex justify-center font-bold text-white">
-    
-    <div class="wins bg-green-600 py-1 px-6 rounded">
-      <p>0</p>
-      <span>WINS</span>                 
-    </div>
-
-    <div class="losses bg-red-600 p-1 px-6 rounded">
-      <p>0</p>
-      <span>LOSSES</span>          
-    </div>
-
-    <div class="draws bg-blue-400 p-1 px-6 rounded">
-      <p>0</p>
-      <span>DRAWS</span>          
-    </div>
-
-  </div>  
-
-  <div>   
-    <div class="font-semibold pt-4 justify-center record flex gap-4">
-      <div class="wins text-green-700">
-        <p>0<span> KO WINS</span></p>        
-      </div>
-      <div class="losses text-red-700">
-        <p>0<span> KO LOSSES</span></p>
-      </div>
-    </div>
-  </div>
-
-  <div class="pt-4 text-center">
-    <p class="font-semibold">Lightweight</p>
-    <p>HT- 6' 0", WT- 135 lbs, RCH- 80"</p>
-  </div>
-
-  <div class="mt-3 btn2 text-center">View Fighter</div>
-
-</div>
-</div>
-  </div>
-
-    <div>
-    <div class="relative boxer-card-container p-6 mb-6 rounded max-w-xs bg-gray-300">
+  <!-- fighter card -->
+  <div class="relative boxer-card-container p-6 mb-6 rounded max-w-xs bg-gray-300">
 
 <div class="boxer-name font-bold pb-4 text-center text-gray-700">
   <h2>Wata Bully</h2>
@@ -236,7 +298,7 @@
   <div class="mt-3 btn2 text-center">View Fighter</div>
 
 </div>
-</div>
   </div>
+
 </template>
 
--- pages/Fighters/index.vue
+++ pages/Fighters/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto gap-6 flex flex-wrap">
+  <div class="container justify-center gap-6 flex flex-wrap ">
     <FighterCard />
   </div>
 </template>
