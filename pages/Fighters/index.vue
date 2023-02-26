3ded45b468a55c43028cb50b4b40afe992e24ce7
Vince489 <vincentcornelius489@gmail.com>
2023-02-25 22:39:21 -0500

    where

--- pages/Fighters/index.vue
+++ pages/Fighters/index.vue
@@ -1,26 +1,12 @@
 <template>
 
-  <div class="container p-8">
-    <div class="p-2">
-      <h2>Search and Sort features coming soon...</h2>
-    </div>
-    <div class="p-2">
-      <h3 class="font-bold">
-        Boxer Records are updated weekly and posted on Mondays.
-      </h3>
-      <h2 class="pt-2 font-semibold">
-        Expect to see the results of bouts conducted by midnight on Fridays to be posted by the following Monday.
-      </h2>
-    </div>
-  </div>
-  
   
 
   <div class="container mt-10 justify-center gap-6 flex flex-wrap">
-    <div v-for="g in gamers">
+    <div v-for="g in gamers"></div>
       <Fighters :gamer="g" />
-    </div>
   </div>
+
 
 </template>
 
